2cb02916f88b5a6af657565f5e5073a980ea31da
Gurmeet Singh <gur3660@gmail.com>
2021-02-19 18:48:19 +0530

    -- auth gurad for main module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.
 import { SignInComponent } from './auth/sign-in/sign-in.component'
 import { SignUpComponent } from './auth/sign-up/sign-up.component';
 import { AUTH_PATH, MAIN_PATHS } from './constants';
+import { MainGuard } from './core/auth-guard-service/main.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: AUTH_PATH.SIGN_IN, pathMatch: 'full' },
   { path: AUTH_PATH.SIGN_IN, component: SignInComponent },
   { path: AUTH_PATH.SIGN_UP, component: SignUpComponent },
   { path: AUTH_PATH.FORGOT_PASSWORD, component: ForgotPasswordComponent },
-  { path: MAIN_PATHS.MAIN, loadChildren: () => import('./main/main.module').then((m) => m.MainModule) },
+  { path: MAIN_PATHS.MAIN,canActivate:[MainGuard], loadChildren: () => import('./main/main.module').then((m) => m.MainModule) },
   { path: '**', pathMatch: 'full', redirectTo: AUTH_PATH.SIGN_IN }
 ];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { MAIN_PATHS } from './constants';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-boiler-point';
+  constructor(
+    private router:Router
+  ){
+    localStorage.setItem('accessToken','fghfdewwrdeydr3y3rtwy');
+    this.router.navigate([MAIN_PATHS.MAIN])
+  }
 }
